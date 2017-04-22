45197d194c634a9efeeb5ddb90a97d1c86fcf57f
isecret <572524331@qq.com>
2017-04-22 16:54:37 +0800

    完成头部小屏导航样式和天气样式及js事件

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// 导航栏事件 
+
+$(function() {
+	$(".header-menubtn").click(function() {
+		$("nav").css("display", "block" == $("nav").css("display") ? "none" : "block")
+	});
+	$(window).resize(function() {
+		767 > $(window).width() ? $("nav").css("display", "none") : $("nav").css("display", "inline-block")
+	});
+	$(window).on("scroll", function(a) {
+		"inline-block" == $("nav").css("display") && "block" == $(".header-menubtn").css("display") && $("nav").css("display", "none")
+	})
+});
+
+// 天气事件
+function getWeather(){
+	$.post("https://api.isecret.vip",null,function(e){
+		$(".weather-t1").text(e.t1);
+		$(".weather-t2").text(e.t2);
+		$(".weather-city").text(e.city);;
+	},'JSON');
+}
+
+getWeather();
